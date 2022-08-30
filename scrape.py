e613e36b984fab0313b7712f891aa2b07c28f750
Cody Brown <cbrown4858@gmail.com>
2022-08-30 10:56:17 -0600

    Initial commit, script outlines

---
+++ scrape.py
@@ -0,0 +1,2 @@
+# scrapes WhoScored.com for event data
+# https://github.com/Ali-Hasan-Khan/Scrape-Whoscored-Event-Data/blob/main/tutorial.ipynb
---
+++ wavesofattack.py
@@ -0,0 +1,18 @@
+import scrape
+
+class Match():
+    pass
+
+def getEventDataForMatch():
+    return
+
+def getEventDataSliceLocations():
+    # only finding the dividing lines between specified event data
+    return
+
+def exportSegment():
+    # distinguish each wave by: 1) wave number / total waves, 2) wave name
+    # cutout or ignore certain events (e.g. substitutions)
+    # this is where each plot/diagram is built
+    return
+
