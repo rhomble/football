d87e01486663b68fa5daec29c25e3b8967457500
Cody Brown <cbrown4858@gmail.com>
2022-09-01 21:09:38 -0600

    README and ideation comments

---
+++ README.md
@@ -0,0 +1,6 @@
+A series of diagrams that each displays all the actions in an attack, from in-possesion until out-possession.
+
+What determines when possession is fully gained and when is it fully lost? 
+Sometimes one event action is a player intercepting the ball but then immediately loses possession of that interception.
+
+A script that prints diagrams as image files for each wave of attack from a match into a directory in local storage
--- wavesofattack.py
+++ wavesofattack.py
@@ -1,18 +1,22 @@
-import scrape
+from scrape import *
 
 class Match():
     pass
 
 def getEventDataForMatch():
+    # the scraper
     return
 
 def getEventDataSliceLocations():
     # only finding the dividing lines between specified event data
     return
 
-def exportSegment():
+def buildSegment():
     # distinguish each wave by: 1) wave number / total waves, 2) wave name
     # cutout or ignore certain events (e.g. substitutions)
     # this is where each plot/diagram is built
+    return
+
+def exportSegment():
     return
 
